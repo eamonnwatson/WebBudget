3daf5e7df927f2b6b44e7480fc6e4a59bb669fa0
Eamonn Watson <Eamonn.Watson@flyacva.com>
2026-02-16 14:35:58 -0400

    Added some Interfaces

---
+++ src/PredictiveBudget.Application/Common/IBudgetPlanRepository.cs
@@ -0,0 +1,12 @@
+﻿using PredictiveBudget.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PredictiveBudget.Application.Common;
+
+public interface IBudgetPlanRepository
+{
+    Task<BudgetPlan?> GetAsync(Guid planId, CancellationToken ct);
+    Task SaveAsync(BudgetPlan plan, CancellationToken ct);
+}
---
+++ src/PredictiveBudget.Application/Common/IClock.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PredictiveBudget.Application.Common;
+
+public interface IClock
+{
+    DateOnly Today();
+}
